389d5e1c021360569f93ca288ae86f2d049382a7
DanEager19 <81655709+DanEager19@users.noreply.github.com>
2023-04-18 15:48:10 -0500

    Added service file

--- scripts/test_keys.rb
+++ scripts/test_keys.rb
@@ -2,7 +2,7 @@
 require 'digest'
 require 'json'
 
-json_data = JSON.parse(File.read('file_input.json').strip)
+json_data = JSON.parse(File.read('../config/file_input.json').strip)
 dir = json_data['dir']
 key = json_data['key']
 checks = json_data['checks']
@@ -23,6 +23,12 @@ r = /(Yes|No)/
 checksum ? checksum_output = 'Yes': checksum_output = 'No'
 
 File.write(scored_file, File.read(scored_file).sub(r, checksum_output))
+
+File.open('../config/counter.txt', 'r+') do |file|
+    num = file.gets.to_i
+    file.rewind
+    file.puts(num.next)
+end
 
 File.delete(key_path)
 check_paths.each do |check|
--- scripts/verify_file.rb
+++ scripts/verify_file.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/ruby
 require 'time'
-require 'json'
 
-json_data = JSON.parse(File.read('file_input.json').strip)
-scored_file = json_data['scored_file']
+while true
+    file_time = File.mtime('../config/counter.txt')
+    now_time = Time.now
 
-file_time = File.mtime(scored_file)
-now_time = Time.now
+    minute_diff = (now_time - file_time).to_i / 60 
 
-minute_diff = (now_time - file_time).to_i / 60 
-
-if minute_diff > 5 then
-    File.delete(scored_file)
+    if minute_diff > 15 then
+        File.delete(scored_file)
+    end
+    
+    sleep(300)
 end
